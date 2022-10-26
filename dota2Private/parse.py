d8d0e16fc10c9692e1d83ba5ebc33935fe162b01
shaurya-gupta24 <77416095+shaurya-gupta24@users.noreply.github.com>
2022-10-26 12:46:25 +0100

    Fixed bug
    fetch_and_write.py was being looped over twice per call

--- dota2Private/parse.py
+++ dota2Private/parse.py
@@ -22,9 +22,75 @@ from privateApp import fetch_and_write
 from privateApp.models import *
 run = 0
 
-
+def fetch_and_write():
+    
+    global run
+    
+    try:
+        Match.objects.all().first()
+    except Exception:
+        connected = False
+        print("database connection failed")
+        connection.close()
+        cursor = connection.cursor()
+    else:
+        connected = True
+    if not run ==429 and connected == True:
+        with open(r'privateApp/lastSequence.txt') as f:
+            maxid = f.readline
+            maxid =maxid()
+        allIds=[]
+        last_seq = 0
+        for i in range(0,9):
+            resd2, matchIds, temp_last_seq = getGamesDota2Api(maxid)
+            if resd2 ==200:
+                allIds+=matchIds
+                last_seq = temp_last_seq
+                maxid=temp_last_seq
+            elif i > 2:
+                break
+            else:
+                time.sleep(2)
+               
+            
+        print("Found "+str(len(allIds))+" matches")
+        if not len(allIds) == 0:
+            print(str(allIds[len(allIds)-1]))
+            chunks = [allIds[x:x+100] for x in range(0, len(allIds), 100)]
+            success=True
+            for chunk in chunks:
+                resstratz, data = getmatchesDetailsStratz(chunk,maxid)
+                run = resstratz
+                if resstratz==200:
+                    add_to_database(data)
+                    
+                    
+                else: 
+                    success=False
+                    break
+                    
+            if success and last_seq!=0:
+                try:
+                    with open('privateApp/lastSequence.txt', "w") as myfile:
+                        myfile.write(last_seq)
+                except Exception as e:
+                    print('File error main loop '+ str(e))
+                    try:
+                        with open('privateApp/lastSequence.txt', "w") as myfile:
+                            myfile.write(last_seq)
+                    except Exception as e:
+                        print('File error main loop '+ str(e))
+        elif resd2 != 200:
+            time.sleep(5)
+        
+    else:
+        if(run==429):
+            rotate_VPN()
+        time.sleep(30)
+        run = 200
+        
 
 
 while True:
-    fetch_and_write.fetch_and_write()
+    fetch_and_write()
     time.sleep(1)
